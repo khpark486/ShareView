3b3adb99afcce4ab53a5780890f47775e6f27775
khpark <khpark486@gmail.com>
2017-06-14 14:44:20 +0900

    ShareView
    create view

---
+++ ShareView/app/src/main/java/pin/parkplue/com/shareview/view/ShareView.java
@@ -0,0 +1,17 @@
+package pin.parkplue.com.shareview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by khpark on 2017-06-14.
+ */
+
+public class ShareView extends RelativeLayout {
+
+    public ShareView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
